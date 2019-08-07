7f73f787b496c2948877ab3f7b9ecd9cb049377e
eyakub <eyakubsorkar@gmail.com>
2019-08-07 08:38:39 +0600

    is anagram that check both words carries same alphabet or not

---
+++ Toph.co/isAnagram.py
@@ -0,0 +1,13 @@
+# both words carries same alphabet or not
+# sort words and check both are equal
+
+def anagramCheck(word1, word2):
+    if(sorted(word1) == sorted()):
+        print('Yes')
+    else:
+        print('No')
+
+
+word1 = input()
+word2 = input()
+anagramCheck(word1, word2)
