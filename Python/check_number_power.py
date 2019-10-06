2175b52cf31645604ca36bf3c4fd0931f90d666b
eyakub <eyakubsorkar@gmail.com>
2019-10-06 12:46:46 +0600

    checkNoPower of 2

--- Python/check_number_power.py
+++ Python/check_number_power.py
@@ -1,13 +1,20 @@
 def checkNoPower(n=16):
-    
+    res = 0
     if n % 2 == 0:
-        count = 0
-        for x in range(0, n+1, 2):
-            s = x
-            if s / 2 == 0:
-                s = s / 2
-                count = count + 1
-                print('count ', count)
+        count = 0                   # count for divisible no of times
+        s = n
+        for x in range(n, 2, -2):   # max range to minimum range 2, that will less -2 as sequence
+            if s % 2 == 0:          # checking no reminder
+                s = s // 2          # divide the number
+                count = count + 1   # count the no of times it's divided
+        
+        for i in range(count):      # as it's checking power of 2, multiply it
+            res = res + (2*2)
+
+    if n == res:
+        print('True')
+    else:
+        print('False')
 
 
-checkNoPower()
+checkNoPower(218)
