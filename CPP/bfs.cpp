92c021f574bf45969a0a27b36daf1bebb554c4cf
eyakub <eyakubsorkar@gmail.com>
2019-10-09 12:19:24 +0600

    TypeError: 'set' object is not subscriptable

--- CPP/bfs.cpp
+++ CPP/bfs.cpp
@@ -1,13 +1,13 @@
 /**
 * a quick implementation of BFS using vectors and queue
 */
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define pb push_back
 
 using namespace std;
 
 vector<bool> v;
-vector<vector<int> > g;
+vector<vector<int>> g;
 
 void edge(int a, int b)
 {
@@ -21,14 +21,17 @@ void bfs(int u)
     q.push(u);
     v[u] = true;
 
-    while(!q.empty()){
+    while (!q.empty())
+    {
         int f = q.front();
         q.pop();
         cout << f << " ";
 
         // Enqueue all adjacent of F and mark them visited
-        for(int i = g[f].begin(); i != g[f].end(); i++){
-            if(!v[*i]){
+        for (int i = g[f].begin(); i != g[f].end(); i++)
+        {
+            if (!v[*i])
+            {
                 q.push(*i);
                 v[*i] = true;
             }
@@ -41,15 +44,18 @@ int main()
     int n, e;
     cin >> n >> e;
     v.assign(n, false)
-    g.assign(n, vector<int>());
+        g.assign(n, vector<int>());
 
     int a, b;
-    for(int i = 0; i < e; i++){
+    for (int i = 0; i < e; i++)
+    {
         cin >> a >> b;
         edge(a, b);
     }
-    for(int i = 0; i < n; i++){
-        if(!v[i]){
+    for (int i = 0; i < n; i++)
+    {
+        if (!v[i])
+        {
             bfs(i);
         }
     }
