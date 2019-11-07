dfcef7e883673c0ac5724b0e733cf062d4f8e790
eyakubsorkar <eyakubsorkar@gmail.com>
2019-11-07 19:27:42 +0600

    diagonal difference work left

---
+++ HackerRank/diagonal_difference.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def diagonalDifference(arr):
+    return arr
+
+
+if __name__ == "__main__":
+    fptr = open(os.environ["OUTPUT_PATH"], "w")
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+    print(result)
