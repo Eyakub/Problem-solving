b0ce4fad524d37bdcc5dddc8cacd1f14c5fcb892
eyakubsorkar <eyakubsorkar@gmail.com>
2019-11-24 16:27:10 +0600

    some python updats

---
+++ Python/properties.py
@@ -0,0 +1,15 @@
+# One common use of a property is to make an attribute read-only.
+
+
+class Pizza:
+    def __init__(self, toppings):
+        self.toppings = toppings
+
+    @property
+    def pineapple_allowed(self):
+        return False
+
+
+pizza = Pizza(["cheese", "tomato"])
+print(pizza.pineapple_allowed)
+pizza.pineapple_allowed = True
