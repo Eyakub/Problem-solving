e5755da81c85802d28e9904ad2838641376e074c
eyakubsorkar <eyakubsorkar@gmail.com>
2019-11-27 18:44:59 +0600

    comment added to understand the problem of drawing book

--- HackerRank/drawing_book.py
+++ HackerRank/drawing_book.py
@@ -1,6 +1,17 @@
 import os
 import sys
 
+"""
+n = 11, p = 4
+every slide has 2 side to divide with 2
+[ ,1][2,3][4,5][6,7][8,9][10,11]
+Total = 11 / 2 = 5
+goal page(from left - right) = 4 / 2
+goal page(right - left0 = total - (left-right)) = 5-2=3
+min(2, 3) = 2
+
+"""
+
 
 def pageCount(n, p):
     end_p = n // 2
