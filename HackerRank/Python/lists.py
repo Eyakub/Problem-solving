7c1e35b1fe26ec559df91fa34815c2b8d701fa93
Eyakub <eyakubsorkar@gmail.com>
2020-02-26 18:06:24 +0600

    hackerrank task left

--- HackerRank/Python/lists.py
+++ HackerRank/Python/lists.py
@@ -1,1 +1,41 @@
-def 
+def insertList(arr):
+    arr = [int(i) for i in arr]
+    print(arr)
+
+def removeList(arr):
+    pass
+
+def popList(arr):
+    pass
+
+def displayList(arr):
+    pass
+
+def sortList(arr):
+    pass
+
+def appendList(arr):
+    pass
+
+def reverseList(arr):
+    pass
+
+
+if __name__ == "__main__":
+    n = int(input())
+    for _ in range(n):
+        arr = input().split(' ')
+        if arr[0] == 'insert':
+            insertList(arr[1:])
+        if arr[0] == 'print':
+            print('print')
+        if arr[0] == 'remove':
+            print('remove')
+        if arr[0] == 'append':
+            pass
+        if arr[0] == 'sort':
+            pass
+        if arr[0] == 'pop':
+            pass
+        if arr[0] == 'reverse':
+            pass
