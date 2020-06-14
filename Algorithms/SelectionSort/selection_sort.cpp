b92c749b259d6be5dfd799e5af7f9eff5dd944eb
Eyakub <eyakubsorkar@gmail.com>
2020-06-14 12:00:01 +0600

    implementation of selection sort using cpp

---
+++ Algorithms/SelectionSort/selection_sort.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+left side <= pivot and right side > pivot
+*/
+
+void selection_sort(int A[], int len);
+
+int main(){
+    // int A[] = {7, 6, 5, 4, 3, 2, 1, 0};
+    int sizeofarray;
+    cout << "Enter the array length: ";
+    cin >> sizeofarray;
+    
+    int A[sizeofarray];
+
+    for (int i = 0; i < sizeofarray; i++){
+        cin >> A[i];
+    }
+    selection_sort(A, sizeofarray);
+    for (int i = 0; i < sizeofarray; i++) cout << A[i] << " ";
+    return 0;
+}
+
+void selection_sort(int A[], int len){
+    int min_index;
+    
+    // One by one move boundary of unsorted subarray
+    for (int i = 0; i < len-1; i++){
+        // find the minimum element in
+        min_index = i;
+        for (int j = i+1; j < len; j++){
+            if (A[j] < A[min_index]){
+                min_index = j;
+            }
+            // swap the found minimum element with the first element
+            swap(A[min_index], A[i]);
+        }
+    }
+}
