c127fc64a534c8f7ab5b1d780709e6592120a7a5
eyakub <eyakubsorkar@gmail.com>
2020-11-03 03:05:26 +0600

    fixed anagram file

--- Python/anagrams.py
+++ Python/anagrams.py
@@ -3,9 +3,9 @@ from collections import Counter
 
 # An anagram is a word or phrase formed by 
 # rearranging the letters of a different word or phrase,
-def checkAnagams():
+def check_anagrams():
     str_1, str_2, str_3 = "acbde", "abced", "abcda"
-    cnt_1, cnt_2, cnt_3  = Counter(str_1), Counter(str_2), Counter(str_3)
+    cnt_1, cnt_2, cnt_3 = Counter(str_1), Counter(str_2), Counter(str_3)
 
     if cnt_1 == cnt_2:
         print('1 and 2 anagram')
@@ -13,4 +13,4 @@ def checkAnagams():
         print('1 and 3 anagram')
 
 
-checkAnagams()
+check_anagrams()
