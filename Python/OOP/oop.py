3fa8029d7c1e04bc0c7b710222752013ef9c86ad
eyakub <eyakubsorkar@gmail.com>
2021-02-17 02:43:41 +0600

    l3-class methods and staticsmethod

--- Python/OOP/oop.py
+++ Python/OOP/oop.py
@@ -20,8 +20,24 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
 
+    @classmethod
+    def set_raise_amount(cls, amount):
+        cls.raise_amount = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split("-")
+        return cls(first, last, pay)
+
+
 emp_1 = Employee('Eyakub', 'Sorkar', 50000)
 emp_2 = Employee('Oyshi', 'Tabassum', 60000)
 
-print(emp_1.num_of_emps)
+emp_str_1 = "Eyakub-Sorkar-70000"
+emp_str_2 = "Oyshi-Tabassum-6000"
+
+new_emp_1 = Employee.from_string(emp_str_1)
+# Employee.set_raise_amount(1.06)
+print(Employee.raise_amount)
+print(new_emp_1.email)
 
