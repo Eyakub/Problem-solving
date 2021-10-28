fa455526684a133eb685cf36898bf9da0e439b78
eyakub <eyakubsorkar@gmail.com>
2021-10-28 07:39:51 +0600

    day3 conditional statement using GO

---
+++ HackerRank/go/src/day3/day_3_conditional_statement.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	NTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	N := int32(NTemp)
+
+	result := solve(N)
+	fmt.Println(result)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func solve(num int32) string {
+	if num%2 != 0 {
+		return "Weird"
+	}
+	if (num >= 2 && num <= 5) || num > 20 {
+		return "Not Weird"
+	}
+	return "Weird"
+}
