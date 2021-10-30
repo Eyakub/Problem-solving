cfa10cd37221396567bb4c90d2c032ffbf0fd3f9
eyakub <eyakubsorkar@gmail.com>
2021-10-30 21:40:20 +0600

    day5, loops using GO

---
+++ HackerRank/go/src/day5/day_5_loops.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int(nTemp)
+	solve(n)
+
+}
+
+func solve(n int) {
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("%d x %d = %d\n", n, i, n*i)
+	}
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
