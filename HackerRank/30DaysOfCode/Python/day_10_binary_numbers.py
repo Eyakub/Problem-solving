6911376402f0a8e68b941e7ead49ed7a5702114d
eyakub <eyakubsorkar@gmail.com>
2021-11-11 23:57:20 +0600

    day10 by python inner function

--- HackerRank/30DaysOfCode/Python/day_10_binary_numbers.py
+++ HackerRank/30DaysOfCode/Python/day_10_binary_numbers.py
@@ -4,10 +4,20 @@ def binary_numbers(n):
         reminder = n % 2
         n = n // 2
         bin_no += str(reminder)
-    return bin_no[::-1]
-        
+
+    def consecutive_max(lst=bin_no[::-1]):
+        cons_list = []
+        count, result = 0, 0
+        for i in range(len(lst)):
+            if lst[i] == '0':
+                count = 0
+            else:
+                count += 1
+                result = max(result, count)
+        print(result)
+    consecutive_max()
 
 
 if __name__ == '__main__':
     n = int(input())
-    print(binary_numbers(n))
+    binary_numbers(n)
