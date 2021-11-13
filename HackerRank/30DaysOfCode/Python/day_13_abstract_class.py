31fc7d5efbd41aad8159ad253f760e8bf3813224
eyakub <eyakubsorkar@gmail.com>
2021-11-14 01:20:54 +0600

    abstraction day 13 solved using cpp and python

---
+++ HackerRank/30DaysOfCode/Python/day_13_abstract_class.py
@@ -0,0 +1,30 @@
+from abc import ABC, ABCMeta, abstractmethod
+
+
+class Book(object, metaclass=ABCMeta):
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    @abstractmethod
+    def display(self):
+        pass
+    
+
+class MyBook(Book):
+    def __init__(self,title,author,price):
+        self.title = title
+        self.author = author
+        self.price = price
+
+    def display(self):
+        print("Title:", title)
+        print("Author:", author)
+        print("Price:",price)
+
+
+title = input()
+author = input()
+price = int(input())
+new_novel = MyBook(title, author, price)
+new_novel.display();
