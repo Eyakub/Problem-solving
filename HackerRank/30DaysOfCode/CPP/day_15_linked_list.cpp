9e897b2db7dc371931a31393fce688836846236e
eyakub <eyakubsorkar@gmail.com>
2021-11-18 02:39:39 +0600

    day 15 linked list using cpp done

--- HackerRank/30DaysOfCode/CPP/day_15_linked_list.cpp
+++ HackerRank/30DaysOfCode/CPP/day_15_linked_list.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 class Node{
     public:
+        // Properties
         int data;
         Node *next;
+        // this constructor will be used to create the forward linked list only
+        // where data will be take the place and next will remain NULL
         Node (int d){
             data = d;
             next = NULL;
@@ -14,7 +17,16 @@ class Node{
 class Solution {
     public:
         Node* insert(Node *head, int data){
-
+            Node* temp = head;
+            // creating new node for the current data where next will be NULL as we don't know the next node yet
+            Node* node = new Node(data);
+            if (!head) return node; // if the head is NULL then return the new node
+            while(temp->next){
+                temp = temp->next;
+            }
+            temp->next = node;  // insert after the last node
+            return head;
+            
         }
         void display(Node *head){
             Node *start = head;
@@ -33,7 +45,6 @@ int main(){
     
     cin >> n;
     while(n-->0){
-        cout << n--> 0 << endl;
         cin >> data;
         head = mylist.insert(head, data);
     }
