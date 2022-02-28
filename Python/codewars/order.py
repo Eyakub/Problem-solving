ecc48426f07d48cf5ca8ce16d0178341de03ccb9
unknown <eyakubsorkar@gmail.com>
2022-02-28 23:44:06 +0600

    order from codewars

--- Python/codewars/order.py
+++ Python/codewars/order.py
@@ -1,15 +1,20 @@
 # https://www.codewars.com/kata/55c45be3b2079eccff00010f/train/python
 
-def order(sentence):
-    final_result = []
+def order1(sentence):
+    final_result = sentence.split()
     for i in sentence.split():
-        min = 1
         for j in i:
             if j.isdigit():
-                print(j)
+                final_result[int(j)-1] = i
+    return ' '.join(final_result)
+
+
+def order(sentence):
+    print('==sorted==', sorted(sentence.split(), key=lambda x: x.split()))
+    return ' '.join(sorted(sentence.split(), key=lambda w:sorted(w)))
 
 
 if __name__ == '__main__':
-    print(order("is2 Thi1s T4est 3a"))
-    # print(order("4of Fo1r pe6ople g3ood th5e the2"))
+    # print(order("is2 Thi1s T4est 3a"))
+    print(order("4of Fo1r pe6ople g3ood th5e the2"))
     # print(order(""))
