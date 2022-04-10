68764daa4dd8c25113ee9345618feb054833da78
unknown <eyakubsorkar@gmail.com>
2022-04-11 02:31:03 +0600

    ...

---
+++ Python/8xGrid.py
@@ -0,0 +1,16 @@
+def solution():
+    pass
+
+def printGrid():
+    pass
+
+def setGrid():
+    pass
+
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        index = input()
+        row, col = index[0], index[1]
+        print("Row-Col: ", row, col)
