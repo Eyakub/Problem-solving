d80fa32ea9e4c080279e532097b25ca1055971d9
eyakub <eyakub.sorkar@goama.com>
2023-01-13 05:41:28 +0600

    updated AES algorithm with authtag

--- Algorithms/Encryption/AES256/AES_256_GCM_with_iv.py
+++ Algorithms/Encryption/AES256/AES_256_GCM_with_iv.py
@@ -10,32 +10,57 @@ password = '703273357638792F423F4428472B4B6250655368566D597133743677397A2443'
 SIGNATURE = None
 
 
-def encrypt(value, key, mode):
-    value = value.encode('ascii')
+# def encrypt(value, key, mode):
+#     value = value.encode('ascii')
+#     iv = Random.new().read(AES.block_size)
+#     cipher = AES.new(key, mode, iv)
+#     ciphertext = cipher.encrypt((value))
+#     global SIGNATURE
+#     SIGNATURE = f"{binascii.hexlify(iv).decode('utf-8')}:{binascii.hexlify(ciphertext).decode('utf-8')}"
+#     return iv + ciphertext
+
+
+# def decrypt(encrypted_value, key, mode):
+#     iv = encrypted_value[:AES.block_size]
+#     cipher = AES.new(key, mode, iv)
+#     return cipher.decrypt(encrypted_value[AES.block_size:])
+
+
+print("\nMessage:\t", plaintext)
+print("Key:\t\t", password)
+
+
+def encrypt(plaintext, key, mode):
     iv = Random.new().read(AES.block_size)
-    cipher = AES.new(key, mode, iv)
-    ciphertext = cipher.encrypt((value))
+    encobj = AES.new(key, mode, iv)
+    ciphertext, authTag = encobj.encrypt_and_digest(plaintext)
+    print("\nInside Encrypt")
+    print("Nonce: ", binascii.hexlify(encobj.nonce))
+    print("AuthTag:", binascii.hexlify(authTag))
+    print("IV: ", binascii.hexlify(iv))
     global SIGNATURE
-    SIGNATURE = f"{binascii.hexlify(iv).decode('utf-8')}:{binascii.hexlify(ciphertext).decode('utf-8')}"
-    return iv + ciphertext
+    SIGNATURE = f"{binascii.hexlify(iv).decode('utf-8')}:{binascii.hexlify(ciphertext).decode('utf-8')}:{binascii.hexlify(encobj.nonce).decode('utf-8')}"
+    return (ciphertext, authTag, encobj.nonce)
 
 
-def decrypt(encrypted_value, key, mode):
-    iv = encrypted_value[:AES.block_size]
-    cipher = AES.new(key, mode, iv)
-    return cipher.decrypt(encrypted_value[AES.block_size:])
+def decrypt(ciphertext, key, mode):
+    (ciphertext, authTag, nonce) = ciphertext
+    encobj = AES.new(key, mode, nonce)
+    print("\nInside Decrypt")
+    print("AuthTag: ", binascii.hexlify(authTag))
+    print("Nonce: ", binascii.hexlify(nonce))
+    return (encobj.decrypt_and_verify(ciphertext, authTag))
 
 
 key = hashlib.sha256(password.encode()).digest()
 
-ciphertext = encrypt(plaintext, key, AES.MODE_GCM)
-
-print("\t\tGCM Mode:")
-print("\nMessage:\t", plaintext)
-print("Key:\t\t", password)
+ciphertext = encrypt(plaintext.encode(), key, AES.MODE_GCM)
 
 res = decrypt(ciphertext, key, AES.MODE_GCM)
 
-print("\n\nEncrypted:\t", ciphertext)
-print("Decrypted:\t", res.decode())
+print("Cipher:\t\t", binascii.hexlify(ciphertext[0]))
+print("Auth Msg:\t", binascii.hexlify(ciphertext[1]))
+print("Nonce:\t\t", binascii.hexlify(ciphertext[2]))
+print("\n\nDecrypted:\t", res.decode())
+
 print("Header Value:\t", SIGNATURE)
