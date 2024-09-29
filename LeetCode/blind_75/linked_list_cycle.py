e8265a84acd191bf91c2cc1a620372fc5e58da71
eyakub <eyakubsorkar@gmail.com>
2024-09-29 12:47:04 +0600

    modified the linked list cycle to check how it works

--- LeetCode/blind_75/linked_list_cycle.py
+++ LeetCode/blind_75/linked_list_cycle.py
@@ -16,3 +16,39 @@ class Solution:
             if slow == fast:
                 return True
         return False
+
+
+def create_linked_list(values, pos):
+    """
+    Creates a linked list from the list of values.
+    If pos >= 0, it creates a cycle linking the last node to the node at index 'pos'.
+    """
+    if not values:
+        return None
+
+    head = ListNode(values[0])   # Create the head of the linked list
+    current = head
+    cycle_node = None
+
+    # Iterate over the values and create nodes
+    for index in range(1, len(values)):
+        current.next = ListNode(values[index])
+        print("Current1", current.next.val)
+        current = current.next
+        print("Current2", current.val)
+
+        # If we are at the position where the cycle should start, store that node
+        if index == pos:
+            cycle_node = current
+            print("Cycle Node", cycle_node.val)
+    # If pos is not -1, create the cycle by connecting the last node to the cycle_node
+    if pos >= 0:
+        current.next = cycle_node
+
+    return head
+
+
+s = Solution()
+linked_list = create_linked_list([3, 1, 2, 0, -4], 2)
+print(linked_list)
+print(s.hasCycle(linked_list))
